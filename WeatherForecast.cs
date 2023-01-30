ba700624fca415fc38709ea551b46eb657fafc69
Matheus Vinicius <devmatheusoxs@gmail.com>
2023-01-30 00:31:44 -0300

    Agendamento de Tarefas com Entity Framework - Ok

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Sistema_agendamento_tarefas_entity_framework;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
